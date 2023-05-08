8233e790c7c79134ccd973ed29d3ba8aed7831cd
Roman Rogschev <omuch13rus@rambler.ru>
2023-05-08 11:24:07 +0200

    initional commit

---
+++ functions.py
@@ -0,0 +1,36 @@
+import json
+from models import Publisher, Book, Shop, Stock, Sale 
+def jsonparser(files):  # Функция получения данных из json файла(для заполнения БД) возвращает список экземпляров классов наших объектов с параметрами в соответствии с моделями 
+    with open(files) as file:
+        r = json.load(file)
+    object_list = []
+    for data in r:
+        if data["model"] == 'publisher':
+            publishers = Publisher(name = data["fields"]["name"])
+            object_list.append(publishers)
+        elif data["model"] == 'book':
+            books = Book(title = data["fields"]["title"], id_publisher = data["fields"]["id_publisher"])
+            object_list.append(books)
+        elif data["model"] == 'shop':
+            shops = Shop(name = data["fields"]["name"])
+            object_list.append(shops)
+        elif data["model"] == 'stock':
+            stocks = Stock(id_shop = data["fields"]["id_shop"], id_book = data["fields"]["id_book"], count = data["fields"]["count"] )
+            object_list.append(stocks)
+        elif data["model"] == 'sale':
+            sales = Sale(price = data["fields"]["price"], date_sale = data["fields"]["date_sale"], id_stock = data["fields"]["id_stock"], count = data["fields"]["count"] )
+            object_list.append(sales)
+    return object_list
+
+def get_information_sale_book(connection ,publish:dict) -> str: # функция получения информации из БД (в соответствии с заданием)
+    query = connection.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale)
+    if publish.keys() == 'name':
+        query = query.filter(Publisher.name == publish['name'])
+    elif publish.keys() == 'id':
+        query = query.filter(Publisher.id == publish['id'])
+    return query.all()
+    
+
+
+
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from models import create_tables
+from functions import jsonparser, get_information_sale_book
+import os
+from dotenv import load_dotenv
+if __name__ == '__main__':
+    load_dotenv()
+    Login_db = os.getenv('Login_db')
+    Password_db = os.getenv('Password_db')
+    Name_db = os.getenv('Name_db')
+    DSN = f"postgresql://{Login_db}:{Password_db}@localhost:5432/{Name_db}" 
+    engine = sqlalchemy.create_engine(DSN) # Создание движка
+    create_tables(engine) # создание таблиц
+    Session = sessionmaker(bind=engine)
+    session = Session() #создание эекземпляра сессии
+    session.add_all(jsonparser('text_file.json')) # заполнение таблицы данными из на основе функции jsonparser модуля funktions
+    print(f'               название книги            |название магазина| стоимость покупки|      дата покупки') # вывод результатов запроса
+    for title, name, price, date_sale in get_information_sale_book(session, {'name':'Pearson'}):  #на основе функции get_information_sale_book модуля funktions
+        print(f'{title:<40} | {name:<15} | {price:<16} | {date_sale}')
+    session.commit()
+    session.close()
